6e70126a2ce27c6ac86ba0d3bf952ff17b3b1f70
Elijah Micho <olum.micho@gmail.com>
2021-01-29 17:09:37 -0700

    created function to handle creating book object and rendering it to DOM

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
+//Variables
 let myLibrary = [];
+const form = document.getElementById('bookform');
 
-function Book() {
 
+//Functions
+function Book(title, author, pages, readStatus) {
+
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.readStatus = readStatus
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(e) {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const readStatus = document.getElementById('readstatus').value
+    
+    e.preventDefault()
+
+    const entry = new Book(title, author, pages, readStatus)
+    
+    myLibrary.push(entry);
+    showCollection()
+    console.log(myLibrary[0]);
     
 }
+
+function showCollection() {
+    const collection = document.getElementById('collection');
+    
+    for(let i = 0; i < myLibrary.length; i++) {
+        collection.innerHTML = `Title of book: ${myLibrary[i].title} by ${myLibrary[i].author}.`
+    }
+}
+
+form.addEventListener('submit', addBookToLibrary);
