20edf42b39d003934397f2a469cec79995a45d66
Elijah Micho <olum.micho@gmail.com>
2021-01-30 20:05:35 -0700

    refactored object creration and pushing entry to library method

--- script.js
+++ script.js
@@ -1,39 +1,69 @@
-//Variables
+//Global Variables
 let myLibrary = [];
 const form = document.getElementById('bookform');
+const renderCollection = localStorage.setItem('collectionEntry', `Title of book: ${''} by ${''}.`);
 
-
-//Functions
-function Book(title, author, pages, readStatus) {
-
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.readStatus = readStatus
+//Book Object
+class Book {
+    constructor(title, author, pages, readStatus) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.readStatus = readStatus;
+    }
 }
 
-function addBookToLibrary(e) {
+//Methods to add/remove to/from library
+function createNewBook() {
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
     const readStatus = document.getElementById('readstatus').value
-    
-    e.preventDefault()
 
-    const entry = new Book(title, author, pages, readStatus)
-    
-    myLibrary.push(entry);
-    showCollection()
-    console.log(myLibrary[0]);
-    
+    return new Book(title, author, pages, readStatus);
 }
 
-function showCollection() {
+function addNewBook(e){
+    e.preventDefault();
+
+    addEntryToLibrary(createNewBook());
+    console.log(myLibrary);
+}
+
+function addEntryToLibrary(newEntry) {
+
+    const { title } = newEntry.title;
+
+    switch(newEntry){
+        case myLibrary.find(book => book.title === title):
+            return false;
+        default:
+            myLibrary.push(newEntry);
+    }
+
+    
+
+    /* const entry = new Book(title, author, pages, readStatus)
+    
+    myLibrary.push(entry);
+
+    const entry_serialized = JSON.stringify(entry);
+
+    localStorage.setItem(`${title}`, entry_serialized)
+
+    const entry_deserialized = JSON.parse(localStorage.getItem(`${title}`));
+
+    console.log(localStorage) */
+}
+
+
+
+function showCollection(title) {
     const collection = document.getElementById('collection');
     
-    for(let i = 0; i < myLibrary.length; i++) {
-        collection.innerHTML = `Title of book: ${myLibrary[i].title} by ${myLibrary[i].author}.`
+    for(let i = 0; i < localStorage.length; i++) {
+        collection.innerHTML = `Title of book: ${localStorage.getItem(`${title}`)} by .author}.`
     }
 }
 
-form.addEventListener('submit', addBookToLibrary);
+form.addEventListener('submit', addNewBook);
